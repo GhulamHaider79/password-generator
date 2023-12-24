d50208e30a64f43e6b54417ab00233483eb8465f
Ghulam Haider <ghulamhaider11201@gmail.com>
2023-12-25 02:59:53 +0500

    develop a password generator

---
+++ app.js
@@ -0,0 +1,57 @@
+var alphabetSmalLetter =("abcdfghijklmnopqrstuvwxyz");
+var alphabetCapLetter =("ABCDEFGHIJKLMNOPQRSTUVXYZ");
+var num = '0123456789';
+var specialCharacter = "!@#$%^&*()_+-=[]{}|;:,./<>?"
+
+
+// var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+// console.log(randomLetter);
+
+var password = "";
+
+function submit (){
+
+    for (var i = 0; i < 4; i++ ){
+
+
+     password = alphabetCapLetter[Math.floor(Math.random () * alphabetCapLetter.length)];   
+     password += alphabetSmalLetter[Math.floor(Math.random () * alphabetSmalLetter.length)];
+     password += alphabetCapLetter[Math.floor(Math.random () * alphabetCapLetter.length)];
+     password += alphabetSmalLetter[Math.floor(Math.random () * alphabetSmalLetter.length)];
+     password += num[Math.floor(Math.random () * num.length)];
+     password += num[Math.floor(Math.random () * num.length)];
+     password += specialCharacter[Math.floor(Math.random () * specialCharacter.length)];
+     password += alphabetSmalLetter[Math.floor(Math.random () * alphabetSmalLetter.length)];
+    }
+    document.getElementById('showPasswordLast').innerHTML = password;
+    // console.log(password);
+}
+
+
+
+
+
+
+function copyToClipboard() {
+    
+    var copyText = document.getElementById("showPasswordLast");
+    var range = document.createRange();
+    range.selectNode(copyText);
+
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+
+    document.execCommand("copy");
+  
+    window.getSelection().removeAllRanges();
+  
+ 
+    console.log(copyText);
+  }
+
+  
+ 
+
+
+
+
